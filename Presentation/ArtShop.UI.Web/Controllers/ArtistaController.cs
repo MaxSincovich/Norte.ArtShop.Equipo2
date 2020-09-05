7ce4f9499e1fdbd4c13a9fa1e8d6a187b25cee07
Carla Rossi Bergara <carla.rossib@gmail.com>
2020-09-05 11:42:43 -0300

    Se  agrega Galeria y Artista

--- Presentation/ArtShop.UI.Web/Controllers/ArtistaController.cs
+++ Presentation/ArtShop.UI.Web/Controllers/ArtistaController.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using System.Web.Mvc;
 namespace ArtShop.UI.Web.Controllers
 {
-    public class Artista_Controller
+    public class Artista_Controller:Controller
     {
+                 
+        public ActionResult Index()
+        {
+            return View();
+        
     }
+
+
+}
 }
--- Presentation/ArtShop.UI.Web/Controllers/GaleriaController.cs
+++ Presentation/ArtShop.UI.Web/Controllers/GaleriaController.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace ArtShop.UI.Web.Controllers
 {
-    public class GaleriaController
+    public class GaleriaController:Controller
     {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
     }
 }
