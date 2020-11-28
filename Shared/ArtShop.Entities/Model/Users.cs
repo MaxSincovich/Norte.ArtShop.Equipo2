35d0c15a70e5f80ec05fa5548a7108d0fc70618b
Sincovich, Maximiliano <maximilianosincovich@hotmail.com>
2020-11-28 15:14:05 -0300

    Cambio usuario a datamember ya que la api le ponia nombre externo

--- Shared/ArtShop.Entities/Model/Users.cs
+++ Shared/ArtShop.Entities/Model/Users.cs
@@ -1,22 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ArtShop.Entities.Model
 {
-    [Serializable]
     public partial class Users
     {
+        [DataMember]
+
         public int IdUsuario { get; set; }
+        [DataMember]
         public string NombreUsuario { get; set; }
+
+        [DataMember]
         public string Contraseña { get; set; }
+
+        [DataMember]
         public string Nombre { get; set; }
+
+        [DataMember]
         public string Apellido { get; set; }
+
+        [DataMember]
         public string DNI { get; set; }
+
+        [DataMember]
         public DateTime FechaNacimiento { get; set; }
+
+        [DataMember]
         public DateTime FechaCreacion { get; set; }
+
+        [DataMember]
         public int IdTipoUsuario { get; set; }
 
     }
