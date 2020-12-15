e2013affa8a5f2d6ab45e5e57002984e5abb7290
Carla Rossi Bergara <carla.rossib@gmail.com>
2020-12-15 17:39:19 -0300

    DAC.

--- Business/ArtShop.Business/CartItemBusiness.cs
+++ Business/ArtShop.Business/CartItemBusiness.cs
@@ -10,31 +10,79 @@ namespace ArtShop.Business
 {
     public class CartItemBusiness
     {
-        private BaseDataService<CartItem> db = new BaseDataService<CartItem>();
-        public List<CartItem> GetProducts()
+        public List<CartItem> List()
         {
-            return db.Get();
-        }
-        public CartItem EditProduct(CartItem cartItem)
-        {
-            return db.Update(cartItem, cartItem.Id);
-        }
-        public CartItem GetById(int id)
-        {
-            return db.GetById(id);
+            List<CartItem> result = default(List<CartItem>);
+            var cartitemDAC = new CartItemDAC();
+            result = cartitemDAC.Select();
+            return result;
         }
 
-        public List<CartItem> GetByIdCart(int idCart)
+
+
+        public void Edit(CartItem cartitem)
         {
-            return db.Get(c => c.CartId == idCart);
+            var cartitemDAC = new CartItemDAC();
+            cartitemDAC.UpdateById(cartitem);
         }
-        public CartItem Create(CartItem cartItem)
+
+
+        public CartItem Get(int id)
         {
-            return db.Create(cartItem);
+            var cartitemDAC = new CartItemDAC();
+            var result = cartitemDAC.SelectById(id);
+            return result;
         }
-        public void Delete(int id)
+
+        public CartItem Add(CartItem cartitem)
         {
-            db.Delete(id);
+            CartItem result = default(CartItem);
+            var cartitemDAC = new CartItemDAC();
+
+            result = cartitemDAC.Create(cartitem);
+            return result;
         }
+
+
+        public void Remove(int id)
+        {
+            var cartitemDAC = new CartItemDAC();
+            cartitemDAC.DeleteById(id);
+        }
+
+
+
+
+        //private BaseDataService<CartItem> db = new BaseDataService<CartItem>();
+        //public List<CartItem> GetProducts()
+        //{
+        //    return db.Get();
+        //}
+
+
+
+        //public CartItem EditProduct(CartItem cartItem)
+        //{
+        //    return db.Update(cartItem, cartItem.Id);
+        //}
+
+
+        //public CartItem GetById(int id)
+        //{
+        //    return db.GetById(id);
+        //}
+
+        //public List<CartItem> GetByIdCart(int idCart)
+        //{
+        //    return db.(c => c.CartId == idCart);
+        //}
+        //public CartItem Create(CartItem cartItem)
+        //{
+        //    return db.Create(cartItem);
+        //}
+        //public void Delete(int id)
+        //{
+        //    db.Delete(id);
+        //}
     }
 }
--- Shared/ArtShop.Entities/Model/CartItem.cs
+++ Shared/ArtShop.Entities/Model/CartItem.cs
@@ -8,27 +8,39 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Runtime.Serialization;
 
 
 namespace ArtShop.Entities.Model
 {
-    
-    
-    
+
+    [DataContract]
     [Serializable]
     
     public partial class CartItem : IdentityBase
     {
+
+        [DataMember]
+        public int Id { get; set; }
+        
+        [DataMember]
         public double Price { get; set; }
 
+        [DataMember]
         public int Quantity { get; set; }
 
+        [DataMember]
         public int CartId { get; set; }
+        
+        [DataMember]
         public int ProductId { get; set; }
 
+        [DataMember]
         public virtual Cart Cart { get; set; }
+
+        [DataMember]
         public virtual Product Product { get; set; }
     }
 }
