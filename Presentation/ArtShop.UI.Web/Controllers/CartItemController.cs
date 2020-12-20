70a956dd4270cae9eae6d7f62aeff3522e82913d
JorgeLRodriguez <jorge-11@live.com.ar>
2020-12-19 21:25:56 -0300

    vista del carrito finalizada

--- Presentation/ArtShop.UI.Web/Controllers/CartItemController.cs
+++ Presentation/ArtShop.UI.Web/Controllers/CartItemController.cs
@@ -24,11 +24,10 @@ namespace ArtShop.UI.Web.Controllers
         }
         public int Getcount()
         {
-            if (System.Web.HttpContext.Current.Session["Cart"] != null && (!String.IsNullOrEmpty(System.Web.HttpContext.Current.Session["Cart"].ToString())))
+            var sessionCart = Session["Cart"];
+            if (sessionCart != null && (!String.IsNullOrEmpty(sessionCart.ToString())))
             {
-                var idCart = System.Web.HttpContext.Current.Session["Cart"].ToString().Split('|');
-                //var listCartItem = CartItemProcess.GetbyCartId(Convert.ToInt32((idCart[1])));
-                return CartItemProcess.GetbyCartId(Convert.ToInt32(idCart[1])).Count();
+                return CartItemProcess.GetbyCartId(Convert.ToInt32(sessionCart.ToString().Split('|')[1])).Count();
             }
             else
             {
@@ -42,6 +41,5 @@ namespace ArtShop.UI.Web.Controllers
             var cartItem = CartItemProcess.GetbyCartId(cartId).Where(a => a.ProductId == idProducto).FirstOrDefault();            
             return cartItem;
         }
-        
     }
 }
