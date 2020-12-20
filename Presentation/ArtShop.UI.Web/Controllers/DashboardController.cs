5b6f5a5c7facbd05053c738845a6c21c1e7b823c
Carla Rossi Bergara <carla.rossib@gmail.com>
2020-12-20 02:54:50 -0300

    Dashboard

---
+++ Presentation/ArtShop.UI.Web/Controllers/DashboardController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using ArtShop.UI.Process;
+using ArtShop.Entities.Model;
+
+namespace ArtShop.UI.Web.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private ProductProcess _pp;
+        private OrderProcess _op;
+
+        public DashboardController()
+        {
+            _pp = new ProductProcess();
+            _op = new OrderProcess();
+        }
+
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Charts()
+        {
+            return View();
+        }
+
+        public ActionResult Tables()
+        {
+            var orders = _op.GetAll();
+
+            return View(orders.OrderByDescending(o => o.OrderDate).ToList());
+        }
+    }
+}
